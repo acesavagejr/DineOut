36464647a3f9f549b026dd538aab8f1df292e922
Entity <85138593+acesavagejr@users.noreply.github.com>
2024-05-20 21:30:16 -0700

    Create Calc.py

---
+++ Calc.py
@@ -0,0 +1,34 @@
+def Calc():
+  print("Calc by EntityAiden")
+  print("-------------------")
+  print("| 1. Addition     |")
+  print("| 2. Subtraction  |")
+  print("| 3. Mutiplcation |")
+  print("| 4. Divition     |")
+  print("-------------------")
+  print()
+  IDK = input()
+
+if IDK == 1
+  NUM1 = input("First number? ")
+  NUM2 = input("Second number? ")
+  print(NUM1 + NUM2)
+  print("prog ended")
+
+if IDK == 2
+  NUM1 = input("First number? ")
+  NUM2 = input("Second number? ")
+  print(NUM1 - NUM2)
+  print("prog ended")
+
+if IDK == 3
+  NUM1 = input("First number? ")
+  NUM2 = input("Second number? ")
+  print(NUM1 * NUM2)
+  print("prog ended")
+
+if IDK == 4
+  NUM1 = input("First number? ")
+  NUM2 = input("Second number? ")
+  print(NUM1 / NUM2)
+  print("prog ended")
