8e836d2f2175790cacfdb7778126b15e774a47e4
Entity <85138593+acesavagejr@users.noreply.github.com>
2024-05-21 18:01:06 -0700

    Update WeatherPlug.py

--- WeatherPlug.py
+++ WeatherPlug.py
@@ -1,9 +1,16 @@
-import os
-os.system("pip install WeatherPlug")
-import WeatherPlug
+def WP():
+    import os
+    from core import Core
+    os.system("pip install WeatherPlug")
+    os.system("clear")
+    from WeatherPlug import WeatherPlug
 
-City = input("Enter a city: ")
-WeatherPlug(City)
-
-print(WeatherPlug.temp)
-print(WeatherPlug.wind)
+    City = input("Enter a city: ")
+    WeatherPlug(City)
+    print(WeatherPlug.temp)
+    print(WeatherPlug.wind)
+    print(WeatherPlug.humidity)
+    print(WeatherPlug.pressure)
+    print(WeatherPlug.desc)
+    print("  ")
+    Core()
