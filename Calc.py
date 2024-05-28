c7894be746b3c7587bb78765d4df8919d9403815
Entity <85138593+acesavagejr@users.noreply.github.com>
2024-05-27 17:21:43 -0700

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -14,30 +14,30 @@ def Calc():
   IDK = input()
 
   if IDK == "1":
-    NUM1 = input("First number? ")
-    NUM2 = input("Second number? ")
+    NUM1 = int(input("First number? "))
+    NUM2 = int(input("Second number? "))
     print(NUM1 + NUM2)
     print("prog ended")
     Core()
 
 
   if IDK == "2":
-    NUM1 = input("First number? ")
-    NUM2 = input("Second number? ")
+    NUM1 = int(input("First number? "))
+    NUM2 = int(input("Second number? "))
     print(NUM1 - NUM2)
     print("prog ended")
     Core()
 
   if IDK == "3":
-    NUM1 = input("First number? ")
-    NUM2 = input("Second number? ")
+    NUM1 = int(input("First number? "))
+    NUM2 = int(input("Second number? "))
     print(NUM1 * NUM2)
     print("prog ended")
     Core()
 
   if IDK == "4":
-    NUM1 = input("First number? ")
-    NUM2 = input("Second number? ")
+    NUM1 = int(input("First number? "))
+    NUM2 = int(input("Second number? "))
     print(NUM1 / NUM2)
     print("prog ended")
     Core()
